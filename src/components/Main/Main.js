b79965cc754552acc2b0193979df2df934f052af
federico73-prog <federico.pagura2@gmail.com>
2021-05-31 18:44:01 -0300

    commit segunda entrega

---
+++ src/components/Main/Main.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import './Main.css';
+import NavBar from '../NavBar/NavBar';
+
+function Main() {
+  return (
+          <div>
+               <NavBar />
+          </div>
+            
+    ); 
+}
+
+export default Main;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Main from './components/Main/Main';
 import reportWebVitals from './reportWebVitals';
+
+import "bootstrap/dist/css/bootstrap.css";
+import {fontAwesomeIcon} from '@fortawesome/react-fontawesome';
+
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Main />
   </React.StrictMode>,
   document.getElementById('root')
 );
