3ac6799b0a655fac253f5ae5cf3ede5ba3adc7ee
federico73-prog <federico.pagura2@gmail.com>
2021-06-14 15:32:35 -0300

    quinto commit

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -11,16 +11,19 @@ const onAdd = (amount) =>{
     }   
 } 
 
-const Item = ({name,value,stock,img}) => {
+const Item = ({id,name,value,stock,img}) => {
      
     // eslint-disable-next-line no-lone-blocks
      return (
 
         <div className="card card__item">
-                    <img src={img}/>
-                    <p>Producto: {name}</p>
-                    <p>Precio: {value}</p>
-                    <p>Stock: {stock}</p>
+                    <img className="img__item" src={img}/>
+                    <div className="div__item">
+                        <p>Producto: {name}</p>
+                        <p>Precio: {value}</p>
+                        <p>Stock: {stock}</p>
+                        <p>Codigo: {id}</p>
+                    </div>
                     <Counter initial={0} stock = {stock} onAdd={onAdd} />
            
         </div>
--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,9 +1,12 @@
 
 import React, { useEffect, useState } from 'react';
 import Item from '../Item/Item';
-import img1 from '../../assets/img/logo.JPG';
-import img2 from '../../assets/img/img5.JPG';
-import img3 from '../../assets/img/img6.JPG';
+import img1 from '../../assets/img/img1.jpg';
+import img2 from '../../assets/img/img2.JPG';
+import img3 from '../../assets/img/img3.JPG';
+import img5 from '../../assets/img/img5.JPG';
+import img6 from '../../assets/img/img6.JPG';
+import img7 from '../../assets/img/img7.JPG';
 import './ItemList.css';
 
 
@@ -13,18 +16,18 @@ const ItemList = () => {
 
     useEffect( () => {
         const productList = [
-            {name:'Producto1', value: 1, stock:20, img : {img1}},
-            {name:'Producto2', value: 2, stock:10, img: {img2}},
-            {name:'Producto3', value: 3, stock:5, img: {img3}},
-            {name:'Producto4', value: 3, stock:5, img: {img3}},
-            {name:'Producto5', value: 3, stock:5, img: {img3}},
-            {name:'Producto6', value: 3, stock:5, img: {img3}},
+            {name:'Producto1', value: 1, stock:20, img : img1},
+            {name:'Producto2', value: 2, stock:10, img: img2},
+            {name:'Producto3', value: 3, stock:5, img: img6},
+            {name:'Producto4', value: 3, stock:5, img: img5},
+            {name:'Producto5', value: 3, stock:5, img: img7},
+            {name:'Producto6', value: 3, stock:5, img: img3},
         ];
 
         const getItems = (productos) =>{
             return new Promise((resolve,reject) => {
                 setTimeout(() => {
-                resolve(productos);
+                    resolve(productos);
                 }, 3000);
             });
         }
@@ -35,10 +38,10 @@ const ItemList = () => {
     return(
             //promise.then(data => {
             <div className="row">
-                {localItems.map((element,i) =>{//aca en vez de productList, iria data
+                {localItems.map((element,i) =>{
                     return (
                         <div className="col-4 row__itemlist">
-                            <Item name={element.name} value = {element.value} stock={element.stock} img ={element.img}/>
+                            <Item id={i} name={element.name} value = {element.value} stock={element.stock} img ={element.img} />
                         </div>
                     );
                 })};
